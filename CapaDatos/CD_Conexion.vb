a8b3acb3f00f00655f5e7f935693ba1d7bef872f
JuniorVelasco <64505683+JuniorVelasco@users.noreply.github.com>
2020-07-19 21:37:16 -0500

    FormularioProveedores
    modificaciones FormularioProveedores

--- CapaDatos/CD_Conexion.vb
+++ CapaDatos/CD_Conexion.vb
@@ -2,12 +2,12 @@
 Imports System.Configuration
 
 Public Class CD_Conexion
-<<<<<<< HEAD
-    Public Shared cnn As String = (ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString)
-    Private Conexion As New SqlConnection(cnn)
-=======
-    Private Conexion As SqlConnection = New SqlConnection("Server=DESKTOP-77HMJDO\SQLEXPRESS;DataBase=DBVENTAS;Integrated Security=true")
->>>>>>> 82df7d6d269a162e60ef4ce5eea69cf68a208ff7
+    Private Conexion As SqlConnection = New SqlConnection("Server=(local);DataBase=DBVENTAS;Integrated Security=true")
+
+    'Public Shared cnn As String = (ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString)
+    'Private Conexion As New SqlConnection(cnn)
+
+
 
     Public Function AbrirConexion() As SqlConnection
         If Conexion.State = ConnectionState.Closed Then
---
+++ SistemaWeb-Ventas/Views/Forms/frmProveedor.aspx.designer.vb
@@ -0,0 +1,114 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class frmProveedor
+
+    '''<summary>
+    '''Control txtDatos.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtDatos As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtruc.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtruc As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtrepresentante.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtrepresentante As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txttelefono.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txttelefono As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Textdireccion.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Textdireccion As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Textemail.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Textemail As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control DropDownList1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents DropDownList1 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Control btnNuevo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnNuevo As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnCancelar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnCancelar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnShowFormCli.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnShowFormCli As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control tblCliente.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents tblCliente As Global.System.Web.UI.WebControls.GridView
+End Class
